37c960c66abfd0d77aba98a5c1f29ebc0e45fd44
zach georgii <zgeorgii@gmail.com>
2016-01-25 13:52:57 -0800

    started equation

---
+++ js/compare.js
@@ -0,0 +1,17 @@
+function hopComp(alphaAcid, citrus, fruity, piney, spicey, floral) {
+  this.alphaAcid = alphaAcid;
+  this.citrus = citrus;
+  this.fruity = fruity;
+  this.piney = piney;
+  this.spicey = spicey;
+  this.floral = floral;
+  // allHops.push(this);
+}
+
+var A = new hopComp(13, 5, 7, 1, 4, 2);
+var B = new hopComp(6, 2, 4, 2, 8, 9);
+var C = new hopComp(9, 7, 1, 8, 5, 3);
+
+function.prototype.compHops(){
+  
+}
