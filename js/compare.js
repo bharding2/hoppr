facd5264c9862bb1eb707da2ea15f21d72c4d2ba
zach georgii <zgeorgii@gmail.com>
2016-01-25 14:11:44 -0800

    prototype

--- js/compare.js
+++ js/compare.js
@@ -12,6 +12,11 @@ var A = new hopComp(13, 5, 7, 1, 4, 2);
 var B = new hopComp(6, 2, 4, 2, 8, 9);
 var C = new hopComp(9, 7, 1, 8, 5, 3);
 
-function.prototype.compHops(){
-  
+hopComp.prototype.findPercentage = function(){
+  Math.abs(Math.abs(A - B) - 68) / 68;
+  this.findPercentage();
 }
+
+// function.prototype.compHops(){
+//
+// }
