8c5e5670a370537d7362a4da7ff3b99333113ab2
Ethan <ethanleebrown@gmail.com>
2016-01-25 16:26:45 -0800

    Added to the description HTML page

--- js/descriptions.js
+++ js/descriptions.js
@@ -5,3 +5,23 @@ for(var i = 0; i < allHops.length; i++) {
   option.value = allHops[i];
   addHopDrop.appendChild(option);
 };
+function handleHopProfile(event) {
+  console.log(event);
+  event.preventDefault();
+
+var addHopName = event.target.name.value;
+var addHopAlphaAcid = event.target.AlphaAcid.value;
+var addCitrus = event.target.Citrus.value;
+var addFruity = event.target.Fruity.value;
+var addPiney = event.target.Piney.value;
+var addSpicy = event.target.Spicy.value;
+var addFloral = event.target.Floral.value;
+
+var newHop = new Hops(addHopName, addHopAlphaAcid, addCitrus, addFruity, addPiney, addSpicy, addFloral);
+console.log(newHop);
+allHops.push(newHop);
+};
+
+var addHopProfile =
+document.getElementById('addhopprofile');
+addHopProfile.addEventListener('submit', handleHopProfile);
